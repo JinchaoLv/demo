b04df8b4895734a83278acf4b511ff683a0ac2ce
JinchaoLv <jinchao.lv@foxmail.com>
2017-11-03 11:43:47 +0800

    none

---
+++ hosts-switcher/src/main/java/com/lvjc/HostsSwitcherApplication.java
@@ -0,0 +1,12 @@
+package com.lvjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostsSwitcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostsSwitcherApplication.class, args);
+	}
+}
